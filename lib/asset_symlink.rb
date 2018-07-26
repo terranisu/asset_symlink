477357a1ea5cc16083af1a07d607034312d980f4
Vadim Kadnikov <terranisu@gmail.com>
2018-07-26 18:11:30 +0200

    Respect Rails.application.config.assets.prefix on symlink creation

--- lib/asset_symlink.rb
+++ lib/asset_symlink.rb
@@ -1,44 +1,50 @@
-require "asset_symlink/version"
-require "asset_symlink/railtie"
+require 'asset_symlink/version'
+require 'asset_symlink/railtie'
 require 'fileutils'
+
 module AssetSymlink
-  def self.execute config
+  def self.execute(config)
     normalize_configuration(config).each do |private_name, public_name|
-      asset = if !Rails.application.assets.nil?
-        Rails.application.assets.find_asset(private_name).digest_path
-      else
-        manifest = if Sprockets::Railtie.respond_to?(:build_manifest)
-          Sprockets::Railtie.build_manifest(Rails.application)
-        else
-          Rails.application.assets_manifest
-        end 
-        manifest.assets[private_name]
-      end
-      digested_location = Rails.root.join('public','assets', asset)
-      public_location = Rails.root.join('public','assets',public_name)
-      if File.dirname(public_name) != '.'
-        FileUtils.mkdir_p(File.dirname(public_location))
-      end
-      digested_location = digested_location.relative_path_from(public_location.dirname)
-      FileUtils.ln_sf(digested_location, Rails.root.join('public','assets',public_name))
+      prefix = Rails.application.config.assets.prefix.sub(%r{^/}, '')
+      asset = find_asset(private_name)
+      digested_path = Rails.root.join('public', prefix, asset)
+      public_path = create_public_path!(public_name)
+      relative_path = digested_path.relative_path_from(public_path.dirname)
+      FileUtils.ln_sf(relative_path, public_path)
     end
   end
 
-
-  def self.normalize_configuration config
+  def self.normalize_configuration(config)
     case config
-    when Hash 
+    when Hash
       config
     when String
-      {config => config}
-    when Array 
-      config.inject({}) do |result, element|
-        result.merge(normalize_configuration(element))
-      end
+      { config => config }
+    when Array
+      config.inject({}) { |a, e| a.merge(normalize_configuration(e)) }
     when NilClass
       {}
     else
       raise ArgumentError, "unexpected item #{config} in config.asset_symlink"
     end
+  end
+
+  def self.find_asset(name)
+    unless Rails.application.assets.nil?
+      return Rails.application.assets.find_asset(name).digest_path
+    end
+    manifest =
+      if Sprockets::Railtie.respond_to?(:build_manifest)
+        Sprockets::Railtie.build_manifest(Rails.application)
+      else
+        Rails.application.assets_manifest
+      end
+    manifest.assets[name]
+  end
+
+  def self.create_public_path!(public_name)
+    path = Rails.root.join('public', 'assets', public_name)
+    FileUtils.mkdir_p(File.dirname(path)) if File.dirname(public_name) != '.'
+    path
   end
 end
--- spec/asset_symlink_spec.rb
+++ spec/asset_symlink_spec.rb
@@ -1,50 +1,64 @@
 require 'spec_helper'
 
 describe AssetSymlink do
-
   describe 'execute' do
-    let(:sandbox) {File.join(File.dirname(__FILE__),'..','tmp')}
-    let(:assets) {Pathname.new(sandbox).join('public', 'assets')}
+    let(:sandbox) { File.join(File.dirname(__FILE__), '..', 'tmp') }
+    let(:assets) { Pathname.new(sandbox).join('public', 'assets') }
+    let(:assets_stub) { double }
+    let(:assets_prefix) { '/assets' }
 
     before(:each) do
-      FileUtils.rm_rf assets, :secure => true
+      FileUtils.rm_rf assets, secure: true
       FileUtils.mkdir_p(assets)
-      @assets_stub = double()
-      Rails.stub_chain(:application,:assets => @assets_stub)
+      Rails.stub_chain(:application, assets: assets_stub)
+      Rails.stub_chain(:application, :config, :assets, prefix: assets_prefix)
       Rails.stub(:root).and_return(Pathname.new(sandbox))
     end
 
     def add_fake_asset(name, location)
-      @assets_stub.stub(:find_asset).with(name).and_return(double(:digest_path => location))
+      assets_stub
+        .stub(:find_asset).with(name).and_return(double(digest_path: location))
       FileUtils.mkdir_p(assets.join(location).dirname)
-      File.open(assets.join(location), 'w'){}
+      File.open(assets.join(location), 'w') {}
     end
 
-    it 'should symlink items at the top level of the assets folder' do
-      add_fake_asset('widget.js', 'widget-abc123.js')
-      AssetSymlink.execute('widget.js')
-      expect(assets.join('widget.js')).to be_a_symlink_to('widget-abc123.js')
+    context 'default assets prefix' do
+      it 'should symlink items at the top level of the assets folder' do
+        add_fake_asset('widget.js', 'widget-abc123.js')
+        AssetSymlink.execute('widget.js')
+        expect(assets.join('widget.js')).to be_a_symlink_to('widget-abc123.js')
+      end
+
+      it 'should create directories as needed' do
+        add_fake_asset('widget.js', 'widget-abc123.js')
+        AssetSymlink.execute('widget.js' => 'v1/foo.js')
+        expect(assets.join('v1', 'foo.js')).to be_a_symlink_to('../widget-abc123.js')
+      end
+
+      it 'should use relative links' do
+        add_fake_asset('external/widget.js', 'external/widget-abc123.js')
+        AssetSymlink.execute('external/widget.js')
+        expect(assets.join('external/widget.js')).to be_a_symlink_to('widget-abc123.js')
+      end
+
+      it 'should overwrite old symlinks' do
+        add_fake_asset('widget.js', 'widget-abc123.js')
+        File.symlink('widget-old.js', assets.join('widget.js'))
+
+        expect(assets.join('widget.js')).to be_a_symlink_to('widget-old.js')
+        AssetSymlink.execute('widget.js')
+        expect(assets.join('widget.js')).to be_a_symlink_to('widget-abc123.js')
+      end
     end
 
-    it 'should create directories as needed' do
-      add_fake_asset('widget.js', 'widget-abc123.js')
-      AssetSymlink.execute('widget.js' => 'v1/foo.js')
-      expect(assets.join('v1','foo.js')).to be_a_symlink_to('../widget-abc123.js')
-    end
+    context 'assets prefix is changed' do
+      let(:assets_prefix) { '/path/to/assets' }
 
-    it 'should use relative links' do
-      add_fake_asset('external/widget.js', 'external/widget-abc123.js')
-      AssetSymlink.execute('external/widget.js')
-      expect(assets.join('external/widget.js')).to be_a_symlink_to('widget-abc123.js')
-    end
-
-    it 'should overwrite old symlinks' do
-      add_fake_asset('widget.js', 'widget-abc123.js')
-      File.symlink('widget-old.js', assets.join('widget.js'))
-
-      expect(assets.join('widget.js')).to be_a_symlink_to('widget-old.js')
-      AssetSymlink.execute('widget.js')
-      expect(assets.join('widget.js')).to be_a_symlink_to('widget-abc123.js')
+      it 'should symlink items at the top level of the assets folder' do
+        add_fake_asset('widget.js', 'widget-abc123.js')
+        AssetSymlink.execute('widget.js')
+        expect(assets.join('widget.js')).to be_a_symlink_to('../path/to/assets/widget-abc123.js')
+      end
     end
   end
 
@@ -54,30 +68,37 @@ describe AssetSymlink do
     end
 
     it 'should convert a single string to a 1 element hash' do
-      AssetSymlink.normalize_configuration('foo.js').should == {'foo.js' => 'foo.js'}
+      AssetSymlink
+        .normalize_configuration('foo.js')
+        .should == { 'foo.js' => 'foo.js' }
     end
 
     it 'should convert an array of strings to hashes' do
-      AssetSymlink.normalize_configuration(%w(foo.js bar.js)).should == {'foo.js' => 'foo.js', 'bar.js' => 'bar.js'}
+      AssetSymlink
+        .normalize_configuration(%w(foo.js bar.js))
+        .should == { 'foo.js' => 'foo.js', 'bar.js' => 'bar.js' }
     end
 
     it 'should return a hash configuration unchanged' do
-      AssetSymlink.normalize_configuration('foo.js' => 'bar.js').should == {'foo.js' => 'bar.js'}
+      AssetSymlink
+        .normalize_configuration('foo.js' => 'bar.js')
+        .should == { 'foo.js' => 'bar.js' }
     end
 
     it 'should convert an array of hashes by merging them' do
-      AssetSymlink.normalize_configuration([{'foo.js' => 'v1/foo.js'}, 
-                                            {'bar.js' => 'v1/bar.js'}]).should == {'foo.js' => 'v1/foo.js',
-                                                                                  'bar.js' => 'v1/bar.js'}
+      AssetSymlink
+        .normalize_configuration([{ 'foo.js' => 'v1/foo.js' }, { 'bar.js' => 'v1/bar.js' }])
+        .should == { 'foo.js' => 'v1/foo.js', 'bar.js' => 'v1/bar.js' }
     end
 
     it 'should allow a mixture of strings and hashes' do
-      AssetSymlink.normalize_configuration(['foo.js', {'bar.js' => 'v1/bar.js'}]).
-                                          should == {'foo.js' => 'foo.js', 'bar.js' => 'v1/bar.js'}
+      AssetSymlink
+        .normalize_configuration(['foo.js', { 'bar.js' => 'v1/bar.js' }])
+        .should == { 'foo.js' => 'foo.js', 'bar.js' => 'v1/bar.js' }
     end
 
     it 'should raise on unexpected item in configuration' do
-      expect {AssetSymlink.normalize_configuration([1])}.to raise_error(ArgumentError)
+      expect { AssetSymlink.normalize_configuration([1]) }.to raise_error(ArgumentError)
     end
   end
 end
